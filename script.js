155b3e46bd539d4864334580684bb83a700295ba
Sheep <50751599+CynicalSheep@users.noreply.github.com>
2023-07-06 21:40:28 +0100

    JS Test

---
+++ script.js
@@ -0,0 +1,13 @@
+function startMenu() {
+  var element = document.getElementById('startMenu');
+  if (element.style.display == "none"){
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+
+}function getCurrentTime() {
+  var now = new Date();
+  var time = now.getHours() + ":" + now.getMinutes().toString().padStart(2, 0);
+  document.getElementById('clock').innerHTML = time;
+}
